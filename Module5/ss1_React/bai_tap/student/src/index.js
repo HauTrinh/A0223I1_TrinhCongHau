874565bcf4a2e649e83c6e298cc9f158f871f761
HauTrinh <hautrinh242@gmail.com>
2023-12-25 20:31:39 +0700

    ss_2_update

--- Module5/ss1_React/bai_tap/student/src/index.js
+++ Module5/ss1_React/bai_tap/student/src/index.js
@@ -3,61 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
-
-const students = [
-  {
-    company: 'Alfreds Futterkiste',
-    contact: 'Maria Anders',
-    country: 'Germany'
-  },
-  {
-    company: 'Centro comercial Moctezuma',
-    contact: 'Francisco Chang',
-    country: 'Mexico'
-  },
-  {
-    company: 'Ernst Handel',
-    contact: 'Roland Mendel',
-    country: 'Austria'
-  },
-  {
-    company: 'Island Trading',
-    contact: 'Helen Bennett',
-    country: 'UK'
-  },
-  {
-    company: 'Laughing Bacchus Winecellars',
-    contact: 'Yoshi Tannamuri',
-    country: 'Canada'
-  },
-  {
-    company: 'Magazzini Alimentari Riuniti',
-    contact: 'Giovanni Rovelli',
-    country: 'Italy'
-  }
-]
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div>
-      <h1> Students</h1>
-      <table>
-        <tr>
-          <th>Company</th>
-          <th>Contact</th>
-          <th>Country</th>
-        </tr>
-        {students.map(student => (
-          <tr>
-              <td>{student.company}</td>
-              <td>{student.contact}</td>
-              <td>{student.country}</td>
-          </tr>
-      ))}
-      </table>
-     
-  </div>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
